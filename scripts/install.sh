23a3f9c27bd296930fc78243bc3bd9c9f8b62e79
Dmitrijs Lapo <dmitry.lapo@hotmail.com>
2024-05-10 13:31:11 +0100

    Fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,17 +3,24 @@
 HOME_DIR="${HOME}"
 DOTFILES_DIR="$(dirname $(pwd))"
 
-CONFIG_FILES=(
+CONFIG_DIRS=(
 	".config/nvim"
 	".config/alacritty"
-	".kube/kubectl_autocompletion"
 	".vim"
+)
+
+CONFIG_FILES=(
+	".kube/kubectl_autocompletion"
 	".bashrc"
 	".git-prompt.sh"
 	".gitignore_global"
 	".prettierrc.json"
 	".tmux.conf"
 )
+
+for dir in "${CONFIG_DIRS[@]}"; do
+	ln -sf "${DOTFILES_DIR}/${dir}" "${HOME_DIR}/$(dirname ${dir})"
+done
 
 for file in "${CONFIG_FILES[@]}"; do
 	ln -sf "${DOTFILES_DIR}/${file}" "${HOME_DIR}/${file}"
