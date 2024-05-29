224193684963847c579782ff6ad1ef3f6db00871
DTLP <dmitry.lapo@hotmail.com>
2024-05-29 20:44:01 +0100

    Add install-vagrant

---
+++ scripts/install-vagrant.sh
@@ -0,0 +1,12 @@
+# Add Virtualbox repo
+sudo sh -c 'echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" > /etc/apt/sources.list.d/virtualbox.list'
+
+# Add the Oracle public key
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+
+sudo apt update
+
+sudo apt install virtualbox-6.1
+
+sudo apt install vagrant
