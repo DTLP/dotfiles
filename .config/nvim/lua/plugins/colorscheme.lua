d266c813472eaef625a8cd158b4f58316ad94072
DTLP <dmitry.lapo@hotmail.com>
2024-07-04 11:10:59 +0100

    Switch to monokai pro

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 return {
   -- add gruvbox
-  { "wittyjudge/gruvbox-material.nvim" },
+  -- { "wittyjudge/gruvbox-material.nvim" },
+  { "tanvirtin/monokai.nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      -- colorscheme = "gruvbox-material",
+      colorscheme = "monokai_pro",
     },
   },
 }
