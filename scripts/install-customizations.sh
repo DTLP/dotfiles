feb29e1fc8485c6cb39947bf6dbaac40cb9796d8
Dmitrijs Lapo <dmitry.lapo@hotmail.com>
2024-10-22 19:46:48 +0100

    Rm Dracula and add scripts for extensions

--- scripts/install-customizations.sh
+++ scripts/install-customizations.sh
@@ -2,38 +2,31 @@
 
 # Gnome Tweaks
 if ! command -v gnome-tweaks &>/dev/null; then
-	sudo apt install gnome-tweaks -y
+  sudo apt install gnome-tweaks -y
 fi
 mkdir -p ~/.themes/
 mkdir -p ~/.icons/
 
-# Dracula theme
-curl -LO https://github.com/dracula/gtk/archive/master.zip
-unzip master.zip -d ~/.themes/
-rm master.zip
-mv ~/.themes/gtk-master/ ~/.themes/Dracula
+cd ~/Downloads/
 
-# Flatery icons
-curl -L -o Flatery-Black.zip https://github.com/cbrnix/Flatery/archive/refs/heads/master.zip
-unzip Flatery-Black.zip
-mv Flatery-master/Flatery-Black-Dark ~/.icons/Flatery-Black-Dark
-mv Flatery-master/Flatery-Dark ~/.icons/Flatery-Dark
-rm -rf Flatery-master
-rm Flatery-Black.zip
+# Open Bar by x10shun
+curl -Lo \
+  openbarneuromorph.v41.shell-extension.zip \
+  https://extensions.gnome.org/extension-data/openbarneuromorph.v41.shell-extension.zip
+gnome-extensions install openbarneuromorph.v41.shell-extension.zip
+rm openbarneuromorph.v41.shell-extension.zip
 
-# Spotify - https://spicetify.app/docs/advanced-usage/installation/#note-for-linux-users
-curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sh
-curl -fsSL https://raw.githubusercontent.com/Astromations/Hazy/main/install.sh | sh
+# Sound Input & Output Device Chooser by kgshank
+curl -Lo \
+  sound-output-device-chooserkgshank.net.v41.shell-extension.zip \
+  https://extensions.gnome.org/extension-data/sound-output-device-chooserkgshank.net.v41.shell-extension.zip
+gnome-extensions install sound-output-device-chooserkgshank.net.v41.shell-extension.zip
+rm sound-output-device-chooserkgshank.net.v41.shell-extension.zip
 
 echo -e "\
 ------------------------------\n\
 Manual steps:
-1. Open up Tweaks / Appearance \n\
-2. Set Applications and Shell to Dracula \n\
-3. Set Icons to Flatery Black Dark \n\
-\n\
-Open Bar by x10shun \n\
-1. Get extension from https://extensions.gnome.org/extension/6580/open-bar \n\
-2. Access Open Bar settings through the Extensions \n\
-3. Import config from configs/openbar \n\
+1. Log out and log back in \n\
+2. Open up 'Extensions' \n\
+3. Enable manually installed extensions \n\
 "
