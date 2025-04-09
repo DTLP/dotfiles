ce329aa356ca205b2dd82333cbd1005e665073c3
Dmitrijs Lapo <dmitry.lapo@hotmail.com>
2025-04-09 14:05:22 +0100

    Add displaying of tabs and spaces

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -6,6 +6,9 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
+vim.opt.listchars = { tab = "→ ", space = "·" }
+vim.opt.list = true
+
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
