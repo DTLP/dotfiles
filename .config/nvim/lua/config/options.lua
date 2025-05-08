1b4bc15216aea5109a818e193b4e06ace6c60906
Dmitrijs Lapo <dmitry.lapo@hotmail.com>
2025-05-08 19:18:04 +0100

    Rm unused

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,19 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
-
--- try this: vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
---
-
-opt.ignorecase = true
-
--- pandoc related
-
-opt.spell = false
-opt.foldmethod = "manual"
-opt.foldenable = false
-
 -- iamcco/markdown-preview
 vim.cmd([[
   function! OpenMarkdownPreview(url)
